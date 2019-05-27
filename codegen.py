edd793e87b5bad8f848dfa695bce311f20ce1227
holycb <chernodub98@gmail.com>
2019-05-27 21:39:45 +0700

    add initial function dictionary in codegen.py

--- codegen.py
+++ codegen.py
@@ -1,7 +1,7 @@
 ## TODO: генерация struct
 ## генерация function
 ## транслировать дефолтные типы переменных (int -> i32, ...) кроме string
-## TODO: генерация объявления внутренних переменных (после типов)
+## генерация объявления внутренних переменных (после типов)
 ## TODO: генерация dowhile, while
 ## TODO: генерация if, else, elif
 ## TODO: придумать что делать со string
@@ -16,27 +16,53 @@ type_dict = {'int': 'i32', 'float': 'double', 'boolean': 'i8'}
 NL = '\n'
 TAB = '    '
 
-# Makes function
-# name - function name
-# type - function type
-# args - function arguments (ex. ['i32 %a', 'double %b'])
-# statements - function statements (ex. ['%0 = add'])
-def llvm_func(name, type, args=[], statements=[]):
-    return [f"define {type} @{name}({', '.join(args)}) {{{NL+TAB}{(NL+TAB).join(statements)}{NL}}}"]
-#print(llvm_func('123', '124', statements=['123', '123']))
+def raiseError(x): raise Exception(x)
+def skip(): pass
 
-# Makes variable definition
-# name - var name 
-# type - var type
-# val - default value
-# valtype - default value type
-def llvm_def(name, type, val=None, valtype=None):
-    default = f'%{name} = alloca {type}'
-    if not val is None:
-        if valtype is None:
-            raise AttributeError('type is not specified')
-        else:
-            return [default, f'store {valtype}, {valtype}* %{name}']
-    else:
-        return [default]
-#print(llvm_def('name', type_dict['float'], 0, type_dict['int']))
+fdict = {
+    'ERROR': lambda: raiseError('error in ast'),
+    'FUNCTION': skip,
+    'STRUCT': skip, 'CONTENT': '', # ?
+    'CONST': '',
+    'ID': '',
+    'ARRAY_ELEMENT': '',
+    'VARIABLE': '',
+    'VARIABLE_ARRAY': '',
+    'TYPE': '',
+    'VALUE': '',
+    'ARRAY_ALLOC': '',
+    'SCOPE': '',
+    'ASSIGN': '',
+    'FUNC_ARGS': '',
+    'FUNCTION': '',
+    'RETURN': '',
+    'EMPTY_STATEMENT': '',
+    'PLUS': '',
+    'MINUS':'',
+    'TIMES': '',
+    'DIVIDE': '',
+    'MODULO': '',
+    'IDIVIDE': '',
+    'BOR': '',
+    'BAND': '',
+    'LT': '',
+    'GT': '',
+    'GE': '',
+    'LE': '',
+    'NE': '',
+    'EQ': '',
+    'LOR': '',
+    'LAND': '',
+    'LNOT': '',
+    'CHAIN_CALL': '',
+    'IF_CONDITION': '',
+    'CONDITION': '',
+    'ELIF': '',
+    'ELSE': '',
+    'DO_WHILE': '',
+    'WHILE': '',
+    'MARK': '',
+    'GOTO': '',
+    'CALL_ARGS': '',
+    'FUNC_CALL': ''
+}
